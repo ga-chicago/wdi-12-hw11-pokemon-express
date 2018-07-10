a186994d60bb18063186dbd1c258149afd03a629
Ron Clark <ronaldtclark@gmail.com>
2018-07-10 17:13:40 -0500

    Commit 4 - connected my database

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -2,15 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000
 
+const Pokemon = require('./models/pokemon')
+
 app.listen(3000, () => {
   console.log('listening on port', port)
 });
 
 
-app.get('/pokemon', (req, res) => {
-  res.send('Welcome to the Pokemon App!')
-})
+// app.get('/pokemon', (req, res) => {
+//   res.send('Welcome to the Pokemon App!')
+// })
 
+app.get('/pokemon', (req, res) => {
+  res.render('index.ejs', {
+    pokemon: Pokemon
+  })
+})
 
 
 
