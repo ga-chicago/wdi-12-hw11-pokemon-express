a75a35ae71e5d271a7c06d9ac61e82c8adedcd93
Badal Moradia <badal@i360llc.com>
2018-07-10 22:49:37 -0500

    Commit 9 - created show views of each pokemon

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -22,6 +22,8 @@ app.get('/pokemon', (req, res) => {
 });
 
 app.get('/pokemon/:id', (req, res) => {
-	res.send(req.params.id);
+	res.render('show.ejs', {
+		pokemon: Pokemon[req.params.id]
+	})
 })
 
