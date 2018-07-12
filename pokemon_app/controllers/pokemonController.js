76f8077e9923e2f6db5708f12aa784521ea5a136
Badal Moradia <badal@i360llc.com>
2018-07-11 21:47:17 -0500

    added Update and New routes

--- pokemon_app/controllers/pokemonController.js
+++ pokemon_app/controllers/pokemonController.js
@@ -8,13 +8,55 @@ const Pokemon = require('../models/pokemon')
 // router.get('', (req, res) => {
 // 	res.send('Welcome to the Pokemon App!');
 // });
-
+//INDEX ROUTE
 router.get('/', (req, res) => {
 	res.render('index.ejs', {
 		pokemon: Pokemon
 	});
 });
 
+
+//CREATE ROUTE
+router.post('/', (req, res) => {
+
+	Pokemon.push(req.body)
+
+	res.redirect('/pokemon/');
+});
+
+
+//NEW ROUTE 
+router.get('/new', (req, res) => {
+	res.render('new.ejs');
+});
+
+
+//EDIT ROUTE 
+router.get('/:id/edit', (req, res) => {
+	res.render('edit.ejs', {
+		pokemon: Pokemon[req.params.id],
+		index: req.params.id
+	});
+
+});
+
+
+//PUT ROUTE
+router.put('/:id', (req, res) => {
+
+	Pokemon[req.params.id] = req.body;
+
+	res.redirect('/pokemon/');
+});
+
+//DELETE ROUTE
+router.delete('/:id', (req, res) => {
+	Pokemon.splice(req.params.id, 1);
+	res.redirect('/pokemon/');
+});
+
+
+//SHOW ROUTE
 router.get('/:id', (req, res) => {
 	res.render('show.ejs', {
 		pokemon: Pokemon[req.params.id]
--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+
+const methodOverride = require('method-override');
+
 const Pokemon = require('./models/pokemon.js');
 
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(methodOverride('_method'));
+
+
 const port = 3000;
+
+
 
 const pokemonController = require('./controllers/pokemonController');
 
